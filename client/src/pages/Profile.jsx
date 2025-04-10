c2a32c85dd25ff633083004e0f135387ca828b10
Ashish Pandey <ash6i2sh@gmail.com>
2025-04-11 00:27:14 +0530

    up

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -27,7 +27,7 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Textarea } from "@/components/ui/textarea";
 import { useFetch } from "@/hooks/useFetch";
-import Loading from "@/components/loading";
+import Loading from "@/components/Loading";
 import usericon from "@/assets/images/user.png";
 import { IoCameraOutline } from "react-icons/io5";
 import Dropzone from 'react-dropzone'
