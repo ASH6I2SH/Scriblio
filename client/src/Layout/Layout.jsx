7cb86303abcbae009a8e6fa327a12f86f71d8413
Ashish Pandey <ash6i2sh@gmail.com>
2025-04-11 15:41:21 +0530

    up

--- client/src/Layout/Layout.jsx
+++ client/src/Layout/Layout.jsx
@@ -8,17 +8,16 @@ import { Outlet } from "react-router-dom";
 const Layout = () => {
   return (
     <SidebarProvider>
-      <div className="flex flex-col min-h-screen w-full">
-        <Topbar />
-        <AppSidebar />
-        
-        {/* Content wrapper that grows to fill space */}
-        <div className="flex-1">
-          <Outlet />
-        </div>
+      <Topbar />
+      <AppSidebar />
+      <main className="w-full min-h-screen flex flex-col">
+  <div className="h-[4rem]"></div>
+  <div className="flex-1">
+    <Outlet />
+  </div>
+  <Footer className="h-[4rem]" />
+</main>
 
-        <Footer />
-      </div>
     </SidebarProvider>
   );
 };
